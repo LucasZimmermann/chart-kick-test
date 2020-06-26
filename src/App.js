523da65b2415c9e2592e4a4a5d1fe8854cc1c7ca
Lucas Zimmermann <lucas.zimmermann@doctolib.com>
2020-06-26 16:29:54 +0200

    stacked bar charts almost done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {
+import Chartkick, {
   LineChart,
   PieChart,
   ColumnChart
@@ -8,41 +8,110 @@ import {
 import 'chart.js'
 
 function App() {
+
+  const MAX_DATA_HEIGHT = 200;
+
+  Chartkick.options = {
+    colors: ["rgb(34, 67, 88)", "rgb(67, 95, 114)", "rgb(118, 145, 161)"]
+  }
+
+  const my_data = [
+    {
+      name: "On site",
+      data: {
+        my_data: 50,
+      }
+    },
+    {
+      name: "TCS",
+      data: {
+        my_data: 25,
+      },
+    },
+    {
+      name: "Mix",
+      data: {
+        my_data: 15,
+      }
+    },
+  ];
+
+  const colleagues_data = [
+    {
+      name: "Me - On site",
+      color: "rgb(204, 236, 250)",
+      data: {
+        colleague_data: 110
+      },
+    },
+    {
+      name: "TCS",
+      color: "rgb(207, 216, 220)",
+      data: {
+        colleague_data: 10
+      },
+    },
+    {
+      name: "Mix",
+      color: "rgb(238, 242, 246)",
+      data: {
+        colleague_data: 30
+      },
+    },
+  ];
+
+  const data = [my_data, colleagues_data];
+
+  const options = {
+    legend: {
+      display: false,
+    },
+    animation: {
+      animateScale: true
+    },
+    tooltips: {
+    },
+    scales: {
+      xAxes: [{
+        display: false,
+        barPercentage: 1,
+        categoryPercentage: 1
+      }],
+      yAxes: [{
+        display: false,
+        ticks: {
+          min: 0,
+          max: MAX_DATA_HEIGHT,
+        },
+        gridLines: {
+          display: false,
+        },
+      }],
+    },
+    borderWidth: 1000,
+  };
+
   return (
     <div className="App">
-      <ColumnChart
-      data = {
-        {
-          "2017-01-01": [6, 11],
-          "2017-01-02": 6
-        }
-      }
-      stacked = {
-        true
-      }
-      />
-      <LineChart data = {
-        {
-          "2017-01-01": 11,
-          "2017-01-02": 6
-        }
-      }
-        width = "800px"
-        height = "500px"
-        colors = {
-          ["#b00", "#666"]
-        }
-      />
-      <PieChart data = {
-        [
-          ["Blueberry", 44],
-          ["Strawberry", 23]
-        ]
-      }
-      colors = {
-        ["#666", "#b00"]
-      }
-      />
+      <div className="space" />
+      <div className="graph">
+        {data.map(data =>
+          <ColumnChart data = {
+            data
+            }
+            dataset = {
+              {
+                borderWidth: 0,
+              }
+            }
+            stacked
+            library={options}
+            width="120px"
+            height="800px"
+            backgroundColor = "rgba(200, 200, 0, 0.1)"
+          />
+        )}
+      </div>
     </div>
   );
 }
